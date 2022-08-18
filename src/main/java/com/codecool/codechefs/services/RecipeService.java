cd40bcbae437155877d99e246cb26e555f7f5f40
kristof553 <kristofmolnar553@gmail.com>
2022-08-18 10:46:48 +0200

    started implement RecipeService

--- src/main/java/com/codecool/codechefs/services/RecipeService.java
+++ src/main/java/com/codecool/codechefs/services/RecipeService.java
@@ -1,4 +1,21 @@
 package com.codecool.codechefs.services;
 
+import com.codecool.codechefs.dao.RecipeDao;
+import com.codecool.codechefs.dao.RecipeStorage;
+import com.codecool.codechefs.models.Recipe;
+
+import java.util.List;
+
 public class RecipeService {
+
+    private RecipeDao recipeDao;
+
+    public RecipeService(RecipeDao recipeDao) {
+        this.recipeDao = recipeDao;
+    }
+
+    public String getAll(){
+        List<Recipe> recipes = recipeDao.getAll();
+        return "";
+    }
 }
