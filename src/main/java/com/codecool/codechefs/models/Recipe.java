5d3dea9bee9a9060ed3307e80ccb3ffa7bc263b8
kristof553 <kristofmolnar553@gmail.com>
2022-08-18 10:19:11 +0200

    init Recipe

---
+++ src/main/java/com/codecool/codechefs/models/Recipe.java
@@ -0,0 +1,46 @@
+package com.codecool.codechefs.models;
+
+public class Recipe {
+
+    private String description;
+
+    private int id;
+
+    private String name;
+
+    private RecipeCategory category;
+
+    private double rating;
+
+
+    public Recipe(String description, String name, RecipeCategory category, double rating) {
+        this.description = description;
+        this.name = name;
+        this.category = category;
+        this.rating = rating;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RecipeCategory getCategory() {
+        return category;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
