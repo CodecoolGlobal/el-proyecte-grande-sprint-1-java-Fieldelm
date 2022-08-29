307d7922c38b2aa7490070dd7696402560402224
kristof553 <kristofmolnar553@gmail.com>
2022-08-29 13:17:14 +0200

    create backend folder

---
+++ backendCodechefs/src/main/java/com/codecool/codechefs/CodechefsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.codechefs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechefsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodechefsApplication.class, args);
+	}
+
+}
