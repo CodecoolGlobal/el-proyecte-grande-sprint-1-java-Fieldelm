26498a80d8dccd154e153ceb6bc7ff5fa012ea59
kristof553 <kristofmolnar553@gmail.com>
2022-08-29 14:09:05 +0200

    connect backend to the frontend

--- frontendCodechefs/frontend-codechefs/src/App.js
+++ frontendCodechefs/frontend-codechefs/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [recipes, setRecipes] = useState()
+
+    useEffect(() => {
+        fetchData()
+            .then(_res => console.log("fetch successful"),
+                _res => console.log("fetch failed"))
+    }, []);
+
+    const fetchData = async () => {
+        let data = await fetch(`/get-all-recepies`)
+        return await data.json()
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
