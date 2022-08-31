aeade15cf09aa9c9acd56fdf226922dc007301c5
reka <debreka@gmail.com>
2022-08-31 14:20:06 +0200

    display image properly

--- frontendCodechefs/frontend-codechefs/src/RecipeCard.js
+++ frontendCodechefs/frontend-codechefs/src/RecipeCard.js
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
 
-const RecipeCard = ({name, description, id}) => {
+const RecipeCard = ({name, description, id, imageURL}) => {
 
     return(
         <Link to={`recipeCard/${id}`} params={{recipeId:{id}}}>
             <div className={"recipeCard"}>
                 <div className={"recipeContainer"}>
+                    <img src={imageURL} alt={name} />
                     <p>{name}</p>
                 </div>
 
