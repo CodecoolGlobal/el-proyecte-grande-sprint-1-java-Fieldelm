1fd12d15f58416832f290b7f6cd741257add637e
reka <debreka@gmail.com>
2022-09-01 09:21:21 +0200

    create fetch method into other module

--- frontendCodechefs/frontend-codechefs/src/RecipePage.js
+++ frontendCodechefs/frontend-codechefs/src/RecipePage.js
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import {useParams} from "react-router-dom";
 import Header from "./Header";
 
 const RecipePage = () => {
 
     const {recipeId} = useParams()
+    const [recipe, setRecipe] = useState();
+
+    useEffect(()=>{
+        
+    })
 
     return(
         <div>
             <Header/>
+
         </div>
     )
 }
