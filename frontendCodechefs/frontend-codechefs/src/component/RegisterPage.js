d3a429a3557b865cd89638eb079f4dc28dcd1f82
kristof553 <kristofmolnar553@gmail.com>
2022-09-26 13:28:57 +0200

    implement register HTML

--- frontendCodechefs/frontend-codechefs/src/component/RegisterPage.js
+++ frontendCodechefs/frontend-codechefs/src/component/RegisterPage.js
@@ -1,6 +1,46 @@
 const RegisterPage = () => {
     return(
-        <h1>register</h1>
+        <div className="container">
+
+            <form id="signup" action="/save_sign_up" method="post">
+
+                <div className="header">
+
+                    <h3>Sign Up</h3>
+
+                    <p>You want to fill out this form</p>
+
+                </div>
+
+                <div className="sep"></div>
+
+                <div className="inputs">
+
+                    <input type="text" name="name" placeholder="name" autoFocus/>
+
+                    <input type="email" name="email" placeholder="e-mail"/>
+
+                    <input type="password" name="password" placeholder="Password"/>
+
+                    <select>
+                        <option>CHEF</option>
+                        <option>USER</option>
+                    </select>
+
+
+                    <div className="checkboxy">
+                        <input name="cecky" id="checky" value="1" type="checkbox"/><label className="terms">I accept the
+                        terms of
+                        use</label>
+                    </div>
+
+                    <input type="submit" value="register"/>
+
+                </div>
+
+            </form>
+
+        </div>
     )
 }
 
