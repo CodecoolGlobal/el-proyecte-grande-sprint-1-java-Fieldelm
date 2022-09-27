2fb82a3b90e81e6397f2a410371bb7ef2f1ff2c6
kristof553 <kristofmolnar553@gmail.com>
2022-09-27 11:09:23 +0200

    register password encode

--- backendCodechefs/src/main/java/com/codecool/codechefs/controller/RegisterController.java
+++ backendCodechefs/src/main/java/com/codecool/codechefs/controller/RegisterController.java
@@ -2,8 +2,10 @@ package com.codecool.codechefs.controller;
 
 
 import com.codecool.codechefs.models.DefaultUser;
+import com.codecool.codechefs.security.PasswordConfig;
 import com.codecool.codechefs.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +16,16 @@ public class RegisterController {
 
     private final UserService userService;
 
+    private final PasswordConfig passwordConfig;
     @Autowired
-    public RegisterController(UserService userService) {
+    public RegisterController(UserService userService, PasswordConfig passwordConfig) {
         this.userService = userService;
+        this.passwordConfig = passwordConfig;
     }
 
     @PostMapping(value = "/register")
     public void registerUser(@RequestBody DefaultUser defaultUser){
-        System.out.println("hali");
+        defaultUser.setPassword(passwordConfig.passwordEncoder().encode(defaultUser.getPassword()));
         userService.saveUser(defaultUser);
     }
 
