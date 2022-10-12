f3e629e060906c712de122c8366b545a924f3a90
reka <debreka@gmail.com>
2022-10-12 10:42:56 +0200

    add price to recipe card

--- frontendCodechefs/frontend-codechefs/src/component/RecipeCardList.js
+++ frontendCodechefs/frontend-codechefs/src/component/RecipeCardList.js
@@ -1,7 +1,7 @@
 import RecipeCard from "./RecipeCard";
 
 const recipeCardList = ({list}) => {
-    const cardList = list.map((recipe)=><RecipeCard key={recipe.id} name={recipe.name} description={recipe.description} id={recipe.id} imageURL={recipe.imageURL} />)
+    const cardList = list.map((recipe)=><RecipeCard key={recipe.id} name={recipe.name} description={recipe.description} id={recipe.id} imageURL={recipe.imageURL} isFree = {recipe.isFree} price = {recipe.price}/>)
     return(
         <div className={"cardContainer"}>
             {cardList}
