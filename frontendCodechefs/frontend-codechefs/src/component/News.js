687e3ed60339208e148bbe60c216a3016d2dc843
kristof553 <kristofmolnar553@gmail.com>
2022-10-12 11:30:01 +0200

    implement flip card

--- frontendCodechefs/frontend-codechefs/src/component/News.js
+++ frontendCodechefs/frontend-codechefs/src/component/News.js
@@ -1,13 +1,22 @@
 const News = () =>{
     return (
         <div className="news-container">
-            <div className="content-container">
+            <div className="news-card ">
+
+                    <div className="flip-card-front">
+                        <h1>This is a news front</h1>
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>This is a news back</h1>
+                    </div>
+
+
+
+            </div>
+            <div className="news-card">
                 <h1>This is a news</h1>
             </div>
-            <div className="content-container">
-                <h1>This is a news</h1>
-            </div>
-            <div className="content-container">
+            <div className="news-card">
                 <h1>This is a news</h1>
             </div>
 
