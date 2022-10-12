917fc0dffff5e0afc25dbe4f89a1b0945b296885
reka <debreka@gmail.com>
2022-10-12 14:33:15 +0200

    add about us

--- frontendCodechefs/frontend-codechefs/src/component/AboutUs.js
+++ frontendCodechefs/frontend-codechefs/src/component/AboutUs.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import Header from './Header';
 
 const AboutUs = () => {
   return (
-    <div>Recipes from professionals</div>
+    <div>
+        <Header/>
+    <div className="recipe-container" >
+    <div className="container__text">
+                    
+                    <h1>Dani</h1>
+                                      
+
+                </div>
+    </div>
+    <div className="recipe-container" >
+    <div className="container__text">
+                    
+                    <h1>Réka</h1>
+                                      
+
+                </div>
+    </div>
+    <div className="recipe-container" >
+    <div className="container__text">
+                    
+                    <h1>Kristóf</h1>
+                                      
+
+                </div>
+    </div>
+    </div>
+    
   )
 }
 
