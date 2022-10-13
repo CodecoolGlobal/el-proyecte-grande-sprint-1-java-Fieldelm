11f62d7fed69907ea4e212dcf372a8128406899d
reka <debreka@gmail.com>
2022-10-13 13:05:39 +0200

    add about us site

--- frontendCodechefs/frontend-codechefs/src/component/AboutUs.js
+++ frontendCodechefs/frontend-codechefs/src/component/AboutUs.js
@@ -5,30 +5,39 @@ const AboutUs = () => {
   return (
     <div>
         <Header/>
+        <div className="about-us-container">
+
     <div className="recipe-container" >
-    <div className="container__text">
+    <div className="about-us__text">
                     
                     <h1>Dani</h1>
+                    <p>cserdniel@hotmail.com</p>
                                       
 
                 </div>
-    </div>
-    <div className="recipe-container" >
-    <div className="container__text">
+  
+   
+    <div className="about-us__text">
                     
                     <h1>Réka</h1>
-                                      
+                      <p>debreka@gmail.com</p>                
 
                 </div>
-    </div>
-    <div className="recipe-container" >
-    <div className="container__text">
+    
+
+    <div className="about-us__text">
                     
-                    <h1>Kristóf</h1>
-                                      
+                    <h1>Kristóf</h1>    
+                      <p>the chef</p>
+                       <p>kristofmolnar553@gmail.com</p>               
 
                 </div>
     </div>
+    <div className="recipe-container" id='motto'>
+      <h1>"Everyone can cook."</h1>
+      <h3>/Gusto/</h3> </div>
+    </div>
+    
     </div>
     
   )
