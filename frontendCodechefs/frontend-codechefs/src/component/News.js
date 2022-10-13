c9fca45d0dc621108f953ab82ef4e876f15db3c7
kristof553 <kristofmolnar553@gmail.com>
2022-10-13 09:50:05 +0200

    feat: flip cards

--- frontendCodechefs/frontend-codechefs/src/component/News.js
+++ frontendCodechefs/frontend-codechefs/src/component/News.js
@@ -1,30 +1,39 @@
 const News = () =>{
     return (
-        <div className="news-container">
-            <div className="news-card news-card1 ">
+        <div>
+            <div className="headline">
+                <h1>Some good advices in the kitchen</h1>
+            </div>
+            <div className="news-container">
+                <div className="news-card news-card1 ">
 
                     <div className="flip-card-front">
                     </div>
                     <div className="flip-card-back">
-                        <h1>This is a news back</h1>
+                        <h1>The fish won't stick to the pan with this trick.</h1>
+                        <button className="news-button">Read</button>
                     </div>
-            </div>
-            <div className=" news-card news-card2">
-                <div className="flip-card-front">
                 </div>
-                <div className="flip-card-back">
-                    <h1>This is a news back</h1>
+                <div className=" news-card news-card2">
+                    <div className="flip-card-front">
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>Therefore, do not heat casseroles in the microwave.</h1>
+                        <button className="news-button">Read</button>
+                    </div>
                 </div>
-            </div>
-            <div className="news-card news-card3">
-                <div className="flip-card-front">
+                <div className="news-card news-card3">
+                    <div className="flip-card-front">
+                    </div>
+                    <div className="flip-card-back">
+                        <h1>This is how to make the perfect and simple chocolate sauce.</h1>
+                        <button className="news-button">Read</button>
+                    </div>
                 </div>
-                <div className="flip-card-back">
-                    <h1>This is a news back</h1>
-                </div>
-            </div>
 
+            </div>
         </div>
+
     )
 }
 
